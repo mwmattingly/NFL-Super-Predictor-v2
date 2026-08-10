26d3789f8ca841ae51ca63fe239447697b5e6110
Michael Mattingly <mwmattingly@gmail.com>
2026-08-10 16:19:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,3 +14,14 @@ st.title("🏈 NFL Simulation Accuracy")
 st.write("HOST =", os.getenv("DATABRICKS_HOST"))
 st.write("CLIENT ID exists =", os.getenv("DATABRICKS_CLIENT_ID") is not None)
 st.write("CLIENT SECRET exists =", os.getenv("DATABRICKS_CLIENT_SECRET") is not None)
+
+conn = sql.connect(
+    server_hostname=os.getenv("DATABRICKS_HOST").replace("https://", ""),
+    http_path=os.getenv("DATABRICKS_HTTP_PATH"),
+    auth_type="oauth-m2m",
+    client_id=os.getenv("DATABRICKS_CLIENT_ID"),
+    client_secret=os.getenv("DATABRICKS_CLIENT_SECRET")
+)
+
+import socket
+socket.setdefaulttimeout(30)
